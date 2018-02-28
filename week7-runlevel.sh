eb8eb20aa29b0b8238ce674be0a874a0cc284099
taimaishuze <35374281+taimaishuze@users.noreply.github.com>
2018-02-27 19:37:50 -0600

    run level script

---
+++ week7-runlevel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Vang Lee
+
+# Week 7 CSCI 2461 Computer Networking 2 - Linux
+# Run level script
+
+echo "Determining user run level......"
+echo ""
+sleep 5
+who -r
+sleep 5
+echo "Mindblown!"
+echo ""
